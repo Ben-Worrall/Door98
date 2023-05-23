a7a37af26043a3becfe9d9717963b1b20608b058
Ben <benworrallpc@gmail.com>
2023-05-23 21:01:07 +0200

    update

---
+++ Doors98.js
@@ -0,0 +1,5 @@
+//power button
+var powerbnt = document.getElementById('powerbnt')
+powerbnt.addEventListener('click', ()=>{
+    document.getElementById('PowerBntICON').style.color = "rgb(75, 196, 4)"
+})
