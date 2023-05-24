7c2f3522787e218724737d34f3f916c81ee43bf2
Ben <benworrallpc@gmail.com>
2023-05-24 12:04:34 +0200

    clicked on powerbnt

--- Doors98.js
+++ Doors98.js
@@ -1,5 +1,37 @@
 //power button
 var powerbnt = document.getElementById('powerbnt')
-powerbnt.addEventListener('click', ()=>{
-    document.getElementById('PowerBntICON').style.color = "rgb(75, 196, 4)"
-})
+powerbnt.addEventListener("mousedown", ClickedDown)
+powerbnt.addEventListener("mouseup", ClickedUp)
+
+
+function ClickedDown(){
+    powerbnt.style.boxShadow = '3vh 2vh 2vh #7a7462, 3vh 2vh 2vh #7a7462'
+    powerbnt.style.height = '32.5vw'
+    powerbnt.style.width = '32.5vw'
+    document.getElementById('PowerBntICON').style.fontSize = "22.5vw"
+     
+     }
+     function ClickedUp(){
+        powerbnt.style.boxShadow = '2vh 1vh 1vh #7a7462, 1vh 1vh 1vh #7a7462'
+        powerbnt.style.height = "35vw"
+        powerbnt.style.width = "35vw"
+        document.getElementById('PowerBntICON').style.fontSize = "25vw"
+
+
+        setTimeout(LoadUpBios, 500) //loadup bios after hitting the power button
+     }
+ 
+ //after hitting the power button then show loadup
+ function LoadUpBios(){
+    //hide power button screen
+document.getElementById("powerBntScreen").style.display = "none"
+document.body.style.backgroundColor = "black"
+
+
+
+
+
+
+
+
+ }
