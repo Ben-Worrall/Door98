98bec2e1bc647e9c750bb10b00920819ff2da7e0
Ben <benworrallpc@gmail.com>
2023-05-25 18:16:37 +0200

    t

--- Doors98bios.js
+++ Doors98bios.js
@@ -259,7 +259,7 @@ function DoorsStartup(){
     window.location = 'doors98Startup/doors98Startup.html'
     }
     //for github
-    else{window.location = 'https://github.com/Ben-Worrall/Doors98/blob/master/doors98Startup/doors98Startup.html'}
+    else{window.location = 'https://github.com/Ben-Worrall/Doors98/blob/master/doors98Startup/doors98Startup'}
     
 }
 
@@ -288,7 +288,7 @@ function DoorsStartupBiosOPT(){
     window.location = 'doors98Startup/doors98Startup.html'
     }
     //for github
-    else if(document.location.protocol == "https:"){window.location = 'https://github.com/Ben-Worrall/Doors98/blob/master/doors98Startup/doors98Startup.html'}
+    else if(document.location.protocol == "https:"){window.location = 'https://github.com/Ben-Worrall/Doors98/blob/master/doors98Startup/doors98Startup'}
     
 
 
