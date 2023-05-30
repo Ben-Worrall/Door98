ef12d4a06f13398f4be3345eacc3bdeef90509ad
Ben <benworrallpc@gmail.com>
2023-05-30 11:07:58 +0200

    homescreen update

---
+++ Doors98Home/doors98Home.js
@@ -0,0 +1,33 @@
+//if user is in fullscreen mode then increase size of buttons and whatnot
+
+
+function fullscreen(){
+    if (window.innerHeight === screen.height) {
+        //windows start button
+        document.getElementById('Doors98START').style.width = "8%"
+       } else {
+        //windows start button
+        document.getElementById('Doors98START').style.width = "7%"
+         
+       }
+
+
+       setInterval(fullscreen, 1);
+}; fullscreen();
+
+
+//time in taskbar
+
+
+function timeTaskBarUpdate(){
+    var time = new Date();
+document.getElementById('hour-min-am-pm').innerText = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+setInterval(timeTaskBarUpdate, 1)
+}
+timeTaskBarUpdate()
+
+
+document.getElementById('Doors98START').addEventListener('click', ()=>{
+    alert('clicked')
+})
