3c36f52800ec277910c164278308f2316c39f68d
Ben <benworrallpc@gmail.com>
2023-06-20 10:28:05 +0200

    shutdown

--- Doors98Home/startmenu/apps-first-opt/shutdown/startmenu-shutdown.js
+++ Doors98Home/startmenu/apps-first-opt/shutdown/startmenu-shutdown.js
@@ -56,25 +56,79 @@ function ShutDown(){
 
 
 let curSelARR = []
-function curSelDetermine(){
-    if(curSelARR[0] = `"Stand by "`){
-        console.log('test1')
-    }else {
-        console.log('no')
-    }
-}
+
 //ok button
 function ok(){
    
 
    
    
-let curSel =  document.querySelector('.startmenu-shutdown-bottom-container input:checked').previousSibling 
+let curSel =  document.querySelector('.startmenu-shutdown-bottom-container input:checked').parentNode.innerText
     
     //console.log(curSel)
     curSelARR[0] = curSel
     console.log(curSelARR[0])
-    curSelDetermine()
+
+
+    if(curSelARR[0] === "Stand by "){  //stand by
+
+        
+
+
+
+
+
+    }else if(curSelARR[0] === "Shut down "){ //shutdown
+
+        
+        //remove the blur
+        for (var i=0; i < allEL.length; i++) {
+            allEL[i].classList.remove("blur") 
+        } 
+        //change location to teh shutdown page 
+        function LoadShutdown(){
+            //for live server
+            if (location.hostname == "localhost" || location.hostname === "127.0.0.1"){
+                window.location = '/doors98Shutdown/doors98shutdown.html'
+            }
+        
+            //for file
+            else if(window.location.protocol == "file:"){
+            window.location = '/doors98Shutdown/doors98shutdown.html'
+            }
+            //for github
+            else if(document.location.protocol == "https:"){window.location = 'https://github.com/Ben-Worrall/Doors98/tree/26beab113a3978db74b33e7af6286038b43fed6a/Doors98Home'}
+        }
+        LoadShutdown()
+
+
+
+
+
+
+
+    }else if(curSelARR[0] === "Restart "){  //restart
+
+        console.log( "restart ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }else if(curSelARR[0] === "Restart in MS-DOS mode "){ //restart in ms mode
+
+        console.log( " restart in ms mode")
+
+
+    }
 
 
 
---
+++ doors98Shutdown/doors98shutdown.js
@@ -0,0 +1,18 @@
+function toPowerBNT(){
+    setTimeout(getTostartbutton, 6500)
+    
+}
+
+function getTostartbutton(){
+    if (location.hostname == "localhost" || location.hostname === "127.0.0.1"){
+        window.location = '/index.html'
+    }
+
+    //for file
+    else if(window.location.protocol == "file:"){
+    window.location = '/index.html'
+    }
+    //for github
+    else if(document.location.protocol == "https:"){window.location = 'https://github.com/Ben-Worrall/Doors98/tree/26beab113a3978db74b33e7af6286038b43fed6a/Doors98Home'}
+}
+
