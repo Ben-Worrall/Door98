a49e6876327267e53bd0b06f1de55b03afc77f5f
Ben <benworrallpc@gmail.com>
2023-06-21 10:49:10 +0200

    shutdown process with cursor style update

--- doors98Shutdown/doors98shutdown.js
+++ doors98Shutdown/doors98shutdown.js
@@ -1,19 +1,21 @@
 let body = document.getElementById('body')
 
 function toPowerBNT(){
+    body.style.cursor = "wait"
     setTimeout(showscreenshutdown, 3000)
     
 }
 
 
 function showscreenshutdown(){
-    
+    body.style.cursor = "none"
     body.classList.add('changebackground')
     
 setTimeout(blackscreenshutdown, 6500)
 }
 
 function blackscreenshutdown(){
+    body.style.cursor = "wait"
     body.classList.remove('changebackground')
     setTimeout(getTostartbutton, 2250)
 }
@@ -22,6 +24,7 @@ function blackscreenshutdown(){
 
 
 function getTostartbutton(){
+    body.style.cursor = "pointer"
     if (location.hostname == "localhost" || location.hostname === "127.0.0.1"){
         window.location = '/index.html'
     }
