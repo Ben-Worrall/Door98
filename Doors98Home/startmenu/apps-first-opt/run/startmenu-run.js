dad93fb7ff328443d8620244b533fd0d3b341a8e
Ben <benworrallpc@gmail.com>
2023-07-05 20:53:46 +0200

    width height and cancel and exot button options

---
+++ Doors98Home/startmenu/apps-first-opt/run/startmenu-run.js
@@ -0,0 +1,19 @@
+
+var StartmenuFirstApps = document.getElementById('StartMenu-popup-first-apps')
+let allEL = document.getElementsByTagName("*");
+
+
+function exitFirstapp(){
+    StartmenuFirstApps.style.display = "none"
+    StartmenuFirstApps.innerHTML = ""
+    
+
+    for (var i=0; i < allEL.length; i++) {
+         
+        allEL[i].classList.remove("blur") 
+            
+    } 
+    
+}
+
+
