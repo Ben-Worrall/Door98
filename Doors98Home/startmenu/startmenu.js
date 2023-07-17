3c6350875aef40566bcc801211223ac2a69633ea
Ben <benworrallpc@gmail.com>
2023-07-17 13:37:34 +0200

    icons for second options

--- Doors98Home/startmenu/startmenu.js
+++ Doors98Home/startmenu/startmenu.js
@@ -153,7 +153,7 @@ Find.onmouseleave = function(){
 //hover over anything else then stop displaying the hovered over stuff
 Body.onmouseover = function(e){
 if(e.target != Find && e.target != Documents && e.target != Favorites && e.target != Programs && e.target != Settings){
-    if(e.target.id != "start-menu-mid" && e.target.id != "StartMenu-popup" && e.target.classList != "main" && e.target.classList != "mainopts" && e.target.classList != "triangleAtag" && e.target.classList != "allow-hover" && e.target.classList != "startmenu-firstopt-icons"){
+    if(e.target.id != "start-menu-mid" && e.target.id != "StartMenu-popup" && e.target.classList != "main" && e.target.classList != "mainopts" && e.target.classList != "triangleAtag" && e.target.classList != "allow-hover" && e.target.classList != "startmenu-icons"){
         secondPopup.style.display="none"
        
     }
