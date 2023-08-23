89c93611a07e7acd1a18b060fa2c96b348750d54
Ben <benworrallpc@gmail.com>
2023-08-23 12:14:40 +0200

    dragging app half way there

--- Doors98Home/doors98Apps.js
+++ Doors98Home/doors98Apps.js
@@ -5,6 +5,79 @@ var TaskbarIconRow = document.querySelectorAll('.dropintaskbar')
 
 
 
+
+
+
+
+
+//drag all the apps around
+
+
+// Make the DIV element draggable:
+function CheckForApps(){
+    let curAppsAll = document.querySelectorAll('.dragableForApps')
+    for(let i =0; i< curAppsAll.length; i++){
+        dragElement(curAppsAll[i])
+    }
+
+
+setInterval(CheckForApps, 1);
+}; CheckForApps();
+;
+
+function dragElement(elmnt) {
+  
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.parentNode.parentNode.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.parentNode.parentNode.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.parentNode.parentNode.style.width = "60vw"
+    elmnt.parentNode.parentNode.style.height = "60vh"
+
+    console.log(elmnt.parentNode)
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+//internet explorer
+
 function internetExplorerLoad(e){
     document.getElementById('StartMenu-popup-second').style.display = "none"
 //console.log(e.parentNode.childNodes[1].childNodes[1].src)
@@ -52,6 +125,12 @@ for(let i = 0; i < TaskbarIconRow.length; i++){
 
     
 }
+
+
+
+
+
+
 
 
 
