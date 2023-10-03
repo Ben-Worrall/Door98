87def962e3ec075ed3157927614495299a191245
Ben <benworrallpc@gmail.com>
2023-10-03 09:09:57 +0200

    test

--- Doors98Home/hoverOnApp.js
+++ Doors98Home/hoverOnApp.js
@@ -50,24 +50,19 @@ function mouseHoverTB(e){
     e = e || window.event;
     e.preventDefault();
 
-
-    //when mosue up, then stop being able to resize app
-   document.onmouseup = CloseElDrag
-
-
-   //if the mouse is between 5 pixels from the edge and the edge, then do mousedown   CurApp.style.cursor = "n-resize"
-
 //for top/bottom level
       if(Math.trunc(curAppSize.left) <= e.clientX <= Math.trunc(curAppSize.right)){
 
          //for top level
         if(e.clientY >= Math.trunc(curAppSize.top)  &&   e.clientY <= Math.trunc(curAppSize.top)+5){
             //console.log('drag up')
+           
             document.body.style.cursor = "n-resize"
 
 
             //for bottom level
         } else if(e.clientY <= Math.trunc(curAppSize.bottom) && e.clientY >= Math.trunc(curAppSize.bottom)-7.5){
+            
             document.body.style.cursor = "n-resize"
 
         }
@@ -77,6 +72,7 @@ function mouseHoverTB(e){
     }else {
         document.body.style.cursor = "default"
       }
+
     
 }
 
@@ -89,23 +85,26 @@ function mouseHoverLR(e){
     e = e || window.event;
     e.preventDefault();
 
-    if(Math.trunc(curAppSize.top) <= e.clientY <= Math.trunc(curAppSize.bottom)){
+
+   if(Math.trunc(curAppSize.top) <= e.clientY <= Math.trunc(curAppSize.bottom)){
 
 
-        //for right level
-        if((curAppSize.right-5) <= e.clientX && e.clientX <=  curAppSize.right ){
-            
-            document.body.style.cursor = "e-resize"
-            
-    
-            //for left level
-        }else if(curAppSize.left <= e.clientX && e.clientX <= (curAppSize.left+5) ){
-           
-            document.body.style.cursor = "e-resize"
-            
-        } else {
-            document.body.style.cursor = "default"
-        }
+    //for right level
+    if((curAppSize.right-5) <= e.clientX && e.clientX <=  curAppSize.right ){
+        
+        document.body.style.cursor = "e-resize"
+        
+
+        //for left level
+    }else if(curAppSize.left <= e.clientX && e.clientX <= (curAppSize.left+5) ){
+        
+        document.body.style.cursor = "e-resize"
+        
+    } 
+
+   
+   }else {
+    document.body.style.cursor = "default"
 }
 }
 
@@ -122,8 +121,7 @@ function mouseHoverDiagonal(e){
     //top left
     if(e.clientY >= curAppSize.top && e.clientY <= (curAppSize.top+12)){
         if(e.clientX >= curAppSize.left && e.clientX <= (curAppSize.left+12)){
-           document.onmousemove = DiagonalTOPleft
-           document.onmouseup = CloseElDrag
+           
            document.body.style.cursor = "nwse-resize"
         }
 
@@ -134,8 +132,7 @@ function mouseHoverDiagonal(e){
     //top right
     if(e.clientY >= curAppSize.top && e.clientY <= (curAppSize.top+12)){
         if(e.clientX <= curAppSize.right && e.clientX >= (curAppSize.right-12)){
-           document.onmousemove = DiagonalTOPright
-           document.onmouseup = CloseElDrag
+           
            document.body.style.cursor = "nesw-resize"
         }
 
@@ -147,8 +144,7 @@ function mouseHoverDiagonal(e){
  
     if(e.clientY <= curAppSize.bottom && e.clientY >= (curAppSize.bottom-12)){
         if(e.clientX >= curAppSize.left && e.clientX <= (curAppSize.left+12)){
-           document.onmousemove = DiagonalBOTTOMleft
-           document.onmouseup = CloseElDrag
+           
            document.body.style.cursor = "nesw-resize"
         }
 
@@ -161,8 +157,7 @@ function mouseHoverDiagonal(e){
 
     if(e.clientY <= curAppSize.bottom && e.clientY >= (curAppSize.bottom-12)){
         if(e.clientX <= curAppSize.right && e.clientX >= (curAppSize.right-12)){
-           document.onmousemove = DiagonalBOTTOMright
-           document.onmouseup = CloseElDrag
+           
            document.body.style.cursor = "nwse-resize"
         }
 
